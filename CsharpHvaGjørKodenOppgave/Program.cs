48232af2edd8ddb77b457d042fdf228f478e4f7b
Henrik005 <henrik.mathias.brathen@gmail.com>
2024-11-29 14:52:18 +0100

    Add project files.

---
+++ CsharpHvaGjørKodenOppgave/Program.cs
@@ -0,0 +1,21 @@
+﻿var range = 250;
+var counts = new int[range];
+string text = "Something";
+while (!string.IsNullOrWhiteSpace(text))
+{
+    text = Console.ReadLine().ToUpper();
+    foreach (var character in text ?? string.Empty)
+    {
+        counts[(int)character]++;
+    }
+    for (var i = 0; i < range; i++)
+    {
+        if (counts[i] > 0)
+        {
+            var character = (char)i;
+            Console.WriteLine(character + " - " + counts[i]);
+                
+        }
+    }
+}
+//Not done yet, the math part is kinda confusing me.
